29c7a35d8a638d36b7a41d3e87b52bf5ab6eb161
huybt222-cell <Huybt222@uef.edu.vn>
2025-10-28 16:50:31 +0700

    fix admin account

--- Areas/Admin/Controllers/UserAdminController.cs
+++ Areas/Admin/Controllers/UserAdminController.cs
@@ -1,116 +1,123 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using JewelryGolden.Models;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
-using JewelryGolden.Models;
 
 namespace JewelryGolden.Areas.Admin.Controllers
 {
-    [Authorize(Roles = "Admin")] // Chỉ Admin mới vào được
+    [Authorize(Roles = "Admin")]
     public class UserAdminController : Controller
     {
         private JewelryDbContext db = new JewelryDbContext();
 
-        // GET: User
+        // ===== Danh sách người dùng =====
         public ActionResult Index()
         {
             var users = db.Users.ToList();
             return View(users);
         }
 
-        // GET: User/Details/5
+        // ===== Chi tiết =====
         public ActionResult Details(string id)
         {
-            if (id == null)
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             var user = db.Users.Find(id);
-            if (user == null)
-                return HttpNotFound();
-
+            if (user == null) return HttpNotFound();
             return View(user);
         }
 
-        // GET: User/Create
+        // ===== Sửa =====
+        public ActionResult Edit(string id)
+        {
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            var user = db.Users.Find(id);
+            if (user == null) return HttpNotFound();
+            return View(user);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,UserName,Email,PhoneNumber")] ApplicationUser model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = db.Users.Find(model.Id);
+                if (user == null) return HttpNotFound();
+
+                user.UserName = model.UserName;
+                user.Email = model.Email;
+                user.PhoneNumber = model.PhoneNumber;
+
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        // ===== Xóa =====
+        public ActionResult Delete(string id)
+        {
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            var user = db.Users.Find(id);
+            if (user == null) return HttpNotFound();
+            return View(user);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            var user = db.Users.Find(id);
+            if (user != null)
+            {
+                db.Users.Remove(user);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: User/Create
+        // ========== TẠO MỚI (POST) ==========
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "UserName,Email,PhoneNumber,PasswordHash")] ApplicationUser user)
         {
             if (ModelState.IsValid)
             {
-                db.Users.Add(user);
-                db.SaveChanges();
+                var userManager = new Microsoft.AspNet.Identity.UserManager<ApplicationUser>(
+                    new Microsoft.AspNet.Identity.EntityFramework.UserStore<ApplicationUser>(db)
+                );
+
+                // Nếu có nhập mật khẩu thì hash đúng chuẩn
+                if (!string.IsNullOrEmpty(user.PasswordHash))
+                {
+                    userManager.Create(user, user.PasswordHash);
+                }
+                else
+                {
+                    // Nếu không nhập mật khẩu, tạo user mà không có mật khẩu
+                    userManager.Create(user);
+                }
+
                 return RedirectToAction("Index");
             }
-            return View(user);
-        }
-
-        // GET: User/Edit/5
-        public ActionResult Edit(string id)
-        {
-            if (id == null)
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-
-            var user = db.Users.Find(id);
-            if (user == null)
-                return HttpNotFound();
 
             return View(user);
         }
 
-        // POST: User/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,UserName,Email,PhoneNumber,PasswordHash")] ApplicationUser user)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(user).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(user);
-        }
 
-        // GET: User/Delete/5
-        public ActionResult Delete(string id)
-        {
-            if (id == null)
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-
-            var user = db.Users.Find(id);
-            if (user == null)
-                return HttpNotFound();
-
-            return View(user);
-        }
-
-        // POST: User/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(string id)
-        {
-            var user = db.Users.Find(id);
-            db.Users.Remove(user);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
 
         protected override void Dispose(bool disposing)
         {
-            if (disposing)
-                db.Dispose();
-
+            if (disposing) db.Dispose();
             base.Dispose(disposing);
         }
     }
+
 }
